7c839270955ca09365d69294398ee7b8834d2ada
Bossit1998 <Bossit98@mail.ru>
2020-10-22 15:11:26 +0500

    init return html template

---
+++ src/main/java/bossit/demo/return_html/ReturnHtmlApplication.java
@@ -0,0 +1,13 @@
+package bossit.demo.return_html;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReturnHtmlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReturnHtmlApplication.class, args);
+    }
+
+}
